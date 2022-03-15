1328fc44321a18fb14531f257ff21449e5e1b2bd
Valkje <loran.knol@gmail.com>
2022-03-15 11:21:14 +0100

    Remove unused files

--- hierarchical_gibbs/alpha_ikPdf.m
+++
@@ -1,8 +0,0 @@
-function density = alpha_ikPdf(alpha_ik, alpha_iks, t_ik, alpha_k, beta_k)
-%ALPHA_IKPDF Summary of this function goes here
-%   Detailed explanation goes here
-    density = (gamma(sum(alpha_iks) + alpha_ik) * t_ik^(alpha_ik - 1)) / ...
-        (gamma(alpha_ik) * alpha_ik^(alpha_k + 1)) * ... 
-        exp(-beta_k / alpha_ik);
-end
-
